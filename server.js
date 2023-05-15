93f877cb09102b6304a2ec747c7752566b97158c
Praveekorn Chianglong <Praveekorn@gmail.com>
2023-05-15 22:07:25 +0700

    tutorial socket.io

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const app = express("");
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.use(express.json());
-app.use(cors());
 
 // app.get('/',(req,res)=>{
 //     res.send('<h1>Hello world</h1>');
@@ -18,14 +11,22 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
+
+io.on('connection', (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit('chat message',msg);
+    console.log("message: " + msg);
   });
 });
 
+
 const port = 8000;
-server.listen(port, () => {
+http.listen(port, () => {
   console.log("Server is running " + port);
 });
